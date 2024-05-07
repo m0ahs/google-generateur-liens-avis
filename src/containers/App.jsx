41dc04470a7c75553247781d8634b5781c9e8737
Joseph Mbai <31988614+m0ahs@users.noreply.github.com>
2024-05-07 13:57:32 +0000

    Ajuster le remplissage interne pour éviter le débordement du contenu
    Affinez l'application du remplissage à l'intérieur du conteneur principal de l'application afin de garantir que le contenu reste centré sans repousser les éléments sur les bords de l'écran. Le remplissage est désormais appliqué de manière sélective aux seules sections nécessaires, ce qui permet de conserver l'intégrité de la mise en page tout en améliorant le confort visuel et la facilité d'utilisation.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,4 @@
 // App.jsx
-
 import React, { useState } from "react";
 import { CSSTransition } from "react-transition-group";
 import "../App.css";
@@ -14,27 +13,29 @@ function App() {
   };
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center bg-cream px-4 sm:px-6 md:px-10 lg:px-16 xl:px-20">
-      <div className="text-center xl:max-w-5xl mx-auto overflow-hidden">
+    <div className="w-screen h-screen flex flex-col justify-center items-center bg-cream">
+      <div className="text-center px-4 md:px-8 lg:px-12 xl:max-w-5xl mx-auto overflow-hidden">
         <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4">
           Générateur de liens d'évaluations Google
         </h1>
-        <p className="mt-2 text-lg mb-6">
+        <p className="text-lg mb-6">
           Obtenez plus d'avis Google en Suisse Romande avec notre générateur de
           lien d'évaluation gratuit. Partagez{" "}
           <span className="font-semibold">l'URL</span> ou le
           <span className="font-semibold"> QR code</span> directement avec vos
           clients.
         </p>
-        <FormHandler toggleHelpSection={toggleHelpSection} />
-        <CSSTransition
-          in={showHelpSection}
-          timeout={300}
-          classNames="help-section"
-          unmountOnExit
-        >
-          <HelpSection />
-        </CSSTransition>
+        <div className="px-4 md:px-6 lg:px-8">  {/* Ajout d'un padding spécifique */}
+          <FormHandler toggleHelpSection={toggleHelpSection} />
+          <CSSTransition
+            in={showHelpSection}
+            timeout={300}
+            classNames="help-section"
+            unmountOnExit
+          >
+            <HelpSection />
+          </CSSTransition>
+        </div>
       </div>
     </div>
   );
