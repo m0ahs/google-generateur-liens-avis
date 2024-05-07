35a8dd74b0cdf3315d952db8693aeb64cd1e9b0c
Joseph Mbai <31988614+m0ahs@users.noreply.github.com>
2024-05-07 17:21:19 +0200

    Update App.jsx

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -33,8 +33,7 @@ function App() {
         <img src="../../arrow-trending-svgrepo-com.svg" 
           alt="Follow instructions"
           id="follow-instruction"
-          className="mx-auto my-4" 
-          style={{ maxWidth: '50px' }}/>
+          className="mx-auto my-4" />
         <FormHandler toggleHelpSection={toggleHelpSection} />
         <CSSTransition
           in={showHelpSection}
