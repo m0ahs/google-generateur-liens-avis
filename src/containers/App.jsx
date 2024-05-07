fde2fca4ee769a76d42e89bdda417dbad057b71a
Joseph Mbai <31988614+m0ahs@users.noreply.github.com>
2024-05-07 12:28:16 +0000

    d

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,9 @@
 // App.jsx
-
 import React, { useState } from "react";
 import { CSSTransition } from "react-transition-group";
 import "../App.css";
 import FormHandler from "../FormHandler";
 import HelpSection from "../components/HelpSection";
-
 function App() {
   const [showHelpSection, setShowHelpSection] = useState(false);
   const toggleHelpSection = () => {
@@ -13,7 +11,7 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen flex justify-center items-center bg-cream mt-8 mb-8">
+    <div className="App h-full flex justify-center items-center bg-cream mt-8 mb-8">
       <div className="text-center px-4 md:px-8 lg:px-12 xl:max-w-5xl mx-auto">
         <h1
           className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4"
@@ -47,4 +45,3 @@ function App() {
   );
 }
 export default App;
-
