eaf85b2033438b5764a31e2e0c78b42d4c496cb4
Joseph Mbai <31988614+m0ahs@users.noreply.github.com>
2024-05-08 12:10:18 +0200

    Update QRCode.jsx

--- src/components/QRCode.jsx
+++ src/components/QRCode.jsx
@@ -3,29 +3,27 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const QRCode = ({ url }) => {
+const QRCode = ({ data }) => {
     const [qrCode, setQrCode] = useState('');
 
     useEffect(() => {
         const fetchQRCode = async () => {
             const response = await axios.get(
-                `https://api.apininjas.com/v1/qrcode?url=${encodeURIComponent(
-                    url
-                )}`,
+                `https://api.api-ninjas.com/v1/qrcode?data=${encodeURIComponent(data)}&format=png`,
                 {
                     headers: {
                         'X-Api-Key': 'iiLpllQ9MDcmj7JO1c4YGg==SeMGDY7pF1ydwXwu',
                     },
                 }
             );
-            setQrCode(response.data.image);
+            setQrCode(response.data);
         };
 
         fetchQRCode();
-    }, [url]);
+    }, [data]);
 
     return (
-        <img src={qrCode} alt="QR Code" />
+        <img src={`data:image/png;base64,${qrCode}`} alt="QR Code" />
     );
 };
 
