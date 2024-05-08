f925016f13be17f70dc7cc2942fc243b34614cb5
Joseph Mbai <31988614+m0ahs@users.noreply.github.com>
2024-05-08 11:10:55 +0000

    Update QR files

--- src/components/QRCode.jsx
+++ src/components/QRCode.jsx
@@ -8,23 +8,31 @@ const QRCode = ({ data }) => {
 
     useEffect(() => {
         const fetchQRCode = async () => {
-            const response = await axios.get(
-                `https://api.api-ninjas.com/v1/qrcode?data=${encodeURIComponent(data)}&format=png`,
-                {
-                    headers: {
-                        'X-Api-Key': 'iiLpllQ9MDcmj7JO1c4YGg==SeMGDY7pF1ydwXwu',
-                    },
-                }
-            );
-            setQrCode(response.data);
+            try {
+                const response = await axios.get(
+                    `https://api.api-ninjas.com/v1/qrcode?data=${encodeURIComponent(data)}&format=svg&size=300`, // Utilisation du format SVG et augmentation de la taille
+                    {
+                        headers: {
+                            'X-Api-Key': 'iiLpllQ9MDcmj7JO1c4YGg==SeMGDY7pF1ydwXwu',
+                            'Accept': 'image/svg+xml'  // S'assurer que l'API retourne le contenu au format SVG
+                        },
+                        responseType: 'blob'  // Important pour le traitement des données binaires pour SVG
+                    }
+                );
+                const url = URL.createObjectURL(response.data);
+                setQrCode(url);
+            } catch (error) {
+                console.error("Error fetching QR code: ", error);
+            }
         };
 
         fetchQRCode();
     }, [data]);
 
     return (
-        <img src={`data:image/png;base64,${qrCode}`} alt="QR Code" className='w-full h-full' />
+        <img src={qrCode} alt="QR Code" style={{ width: '100%', height: 'auto' }} /> // Retirer les classes Tailwind pour le contrôle direct via style
     );
 };
 
 export default QRCode;
+
